c3586917959b713f208f8db21c7aa315af6acef6
aakashabaper69 <aakashkrash46@gmail.com>
2026-02-22 17:58:19 +0000

    fix CORS for preflight

--- api/latency.py
+++ api/latency.py
@@ -4,6 +4,14 @@ from pydantic import BaseModel
 import json
 import numpy as np
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app = FastAPI()
 
 app.add_middleware(
